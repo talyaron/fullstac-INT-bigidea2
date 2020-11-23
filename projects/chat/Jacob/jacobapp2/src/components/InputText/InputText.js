112009f9a57953ca3e2947efe51b9b3e16c06a0e
Jacob Karlovsky <jacobka@sas.upenn.edu>
2020-11-23 10:57:05 +0200

    update

---
+++ projects/chat/Jacob/jacobapp2/src/components/InputText/InputText.js
@@ -0,0 +1,28 @@
+import './InputText.css'
+import { DB } from '../../firebase/firebaseConfig.js'
+
+function InputText() {
+
+    function handleSubmit(e) {
+        try {
+            e.preventDefault()
+
+            const inputText = (e.target.children.input.value)
+
+            DB.collection('messages').add({ message: inputText, time: new Date().getTime() })
+                .then(() => { console.log('message was saved') })
+                .catch(e => { console.error(e) })
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
+    return (
+        <form className='InputText' onSubmit={handleSubmit}>
+            <textarea name='input' placeholder='Write a message' />
+            <button type='submit'>Send</button>
+        </form>
+    )
+}
+
+export default InputText
