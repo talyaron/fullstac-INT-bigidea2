91e4db33aa3b15e1f4df98a960abe35076a5001c
Abby <abbymiller0123@gmail.com>
2020-11-25 14:16:23 +0200

    a

--- projects/hangman/abbyhangman/src/App.js
+++ projects/hangman/abbyhangman/src/App.js
@@ -3,59 +3,103 @@ import { DB } from './firebaseConfig';
 import { useState } from 'react'
 
 function App() {
+
   const [gameId, setGameId] = useState('')
   const [isHidden, setIsHidden] = useState(false);
+
+  let enteredGameId
+  let name
+  let userUID
+
   function handleInput() {
     //creating a game ID 
     let generatedId = Math.floor(Math.random() * 9000) + 1000;
     console.log(generatedId);
     setGameId(generatedId);
     //adding the collection and game ID to the database 
-    DB.collection('games').add({gameId: generatedId})
+    DB.collection('games').add({ gameId: generatedId })
       .then((game) => { console.log(game.id) })
       .catch(e => { console.error(e) })
     //hiding the new game button 
     //document.getElementById('newGame').style.display = "none";
     handleHideMe()
-    handleName()
   }
 
   function handleHideMe() {
     setIsHidden(true)
   }
 
-  function handleName() {
-    let name = document.getElementById('name').value
-    let UID = getUserUID()
-    console.log(name, UID) 
-    sessionStorage.setItem('playerID', UID);
-    DB.collection('games').add({player: name, userID: UID})
+  function handleGameInfo() {
+    gameUpdate()
+    enteredGameId = document.getElementById('gameId').value
+    getUserUID();
+    getName();
+    DB.collection('games').add({ enteredGameId: enteredGameId, userUID: userUID, name: name })
       .catch(e => { console.error(e) })
   }
 
   const uid = function () {
     return Date.now().toString(36) + Math.random().toString(36).substr(2);
   }
-
-  //returns the uid
   function getUserUID() {
-      //get the uid
-      let userUID = sessionStorage.getItem('userUID');
-      if (userUID === null) {
-          userUID = uid();
-          //create a uid
-          sessionStorage.setItem('userUID', userUID);
-      }
-      return userUID;
+    //get the uid
+    userUID = sessionStorage.getItem('userUID');
+    console.log("userUID: " + userUID);
+    if (userUID === null) {
+      userUID = uid();
+      //create a uid
+      sessionStorage.setItem('userUID', userUID);
+    }
+    return userUID;
   }
 
+  function getName() {
+    name = document.getElementById('name').value;
+    console.log(name);
+  }
+  //going to the database 
+  function gameUpdate() {
+    DB.collection('games')
+      .where('gameId', '==', gameId.toString())
+      .limit(1)
+      .get()
+      .then(gamesDB => {
+
+        if (gamesDB.size === 0) {
+          alert("A game with this id doesn't exist")
+        } else {
+          gamesDB.forEach(gameDB => {
+            console.log(gameDB.data())
+
+            let { players } = gameDB.data();
+            console.log(players)
+
+            if (players === undefined) {
+              players = {};
+            }
+            players[userUID] = { name };
+            console.log(players);
+            DB.collection('games').doc(gameDB.id).update({ players })
+              .then(() => { console.log('stored player', userUID, 'in game number', gameDB.id) })
+              .catch(e => {
+                console.error(e)
+              })
+            })
+          }
+        })
+        .catch(e => {
+          console.error(e)
+        })
+
+        } 
 
   return (
     <div className="App">
-      <input type='submit' id='newGame' value='New Game' onClick={handleInput} style={{display: `${isHidden?'none':'block'}`}}/>
+      <input type='submit' id='newGame' value='New Game' onClick={handleInput} style={{ display: `${isHidden ? 'none' : 'block'}` }} />
       <p id='randomNumber'>Game ID: {gameId}</p>
-      <input type='text' id='name' placeholder='Enter Your Name'/>
-      <input type='submit' value='Submit Name' onClick={handleName}/>
+      <input type='text' id='gameId' placeholder='Enter Game ID' />
+      <input type='text' id='name' placeholder='Enter Your Name' />
+      <input type='submit' value='Submit' onClick={handleGameInfo} />
     </div>
   );
 }
