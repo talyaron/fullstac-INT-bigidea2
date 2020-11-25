e9cf430b7cee715da6e4cee5cbc77fa75821bb52
Abby <abbymiller0123@gmail.com>
2020-11-25 11:20:18 +0200

    a

--- projects/hangman/abbyhangman/src/App.js
+++ projects/hangman/abbyhangman/src/App.js
@@ -17,16 +17,45 @@ function App() {
     //hiding the new game button 
     //document.getElementById('newGame').style.display = "none";
     handleHideMe()
+    handleName()
   }
 
   function handleHideMe() {
     setIsHidden(true)
   }
 
+  function handleName() {
+    let name = document.getElementById('name').value
+    let UID = getUserUID()
+    console.log(name, UID) 
+    sessionStorage.setItem('playerID', UID);
+    DB.collection('games').add({player: name, userID: UID})
+      .catch(e => { console.error(e) })
+  }
+
+  const uid = function () {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  }
+
+  //returns the uid
+  function getUserUID() {
+      //get the uid
+      let userUID = sessionStorage.getItem('userUID');
+      if (userUID === null) {
+          userUID = uid();
+          //create a uid
+          sessionStorage.setItem('userUID', userUID);
+      }
+      return userUID;
+  }
+
+
   return (
     <div className="App">
       <input type='submit' id='newGame' value='New Game' onClick={handleInput} style={{display: `${isHidden?'none':'block'}`}}/>
       <p id='randomNumber'>Game ID: {gameId}</p>
+      <input type='text' id='name' placeholder='Enter Your Name'/>
+      <input type='submit' value='Submit Name' onClick={handleName}/>
     </div>
   );
 }
