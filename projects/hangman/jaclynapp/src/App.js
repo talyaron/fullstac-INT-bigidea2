a8209885693ac008e8e668fa903f7b77d1c84c22
Jaclyn <jaclynjosephson@gmail.com>
2020-11-29 10:43:42 +0200

    dsf

--- projects/hangman/jaclynapp/src/App.js
+++ projects/hangman/jaclynapp/src/App.js
@@ -1,83 +1,72 @@
 import './App.css';
 import { DB } from './Firebase/firebaseConfig';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+let number;
 
 function App() {
   const [gameId, setGameId] = useState('');
-  const [showCreateGame, setShowCreateGame] = useState(true);
-
-  function handleNewGame() {
-    let randomId = Math.floor(Math.random() * 10000 + 1);
-    setGameId(randomId);
-    DB.collection('games').add({ gameId: randomId.toString() })
-      .then((game) => { console.log(game.id) })
-      .catch(e => { console.error(e) })
-    setShowCreateGame(false)
-  }
-
-  function joinGame(e) {
-    e.preventDefault();
-
-    let gameId = e.target.children.gameid.value;
-    let username = e.target.children.username.value;
-    console.log(gameId)
-
-    let userId = createUid();
-    sessionStorage.setItem('userId', userId)
-
-    DB.collection('games')
-    .where('gameId','==', gameId.toString())
-    .limit(1)
-    .get()
-    .then(gamesDB=>{
-      gamesDB.forEach(gameDB=>{
-        console.log(gameDB.data())
-        let {players} = gameDB.data();
-        if(players === undefined){
-          players = [userId]
-        } 
-
-        if(!players.includes(userId)){
-          players.push(userId)
-        }
-
-        players[userId] = {username};
-
-        console.log(players)
-        DB.collection('games').doc(gameDB.id).update({players})
-        .then(()=>{console.log('stored player', userId, 'in game number', gameDB.id)})
-        .catch(e=>{
-          console.error(e)
-        })
-      })
-      
-    })
-    .catch(e=>{
-      console.error(e)
-    })
-
-  }
-
-
+  const [isHide, setIsHide] = useState(false);
   return (
+
+
+
     <div className="App">
-      <button id="submit" onClick={handleNewGame} className={showCreateGame ? '' : 'hidden'}>
-        Generate Game Id and Submit
-      </button>
-      <p id="gameId>"> Game ID:{gameId} </p>
-      <form onSubmit={joinGame} >
-        <input type='text' name='gameid' placeholder='Enter game number' />
-        <button type='submit'>Join</button>
-        <input type='text' name='username' placeholder='Enter username' />
-        <button type='submit'>submit</button>
-      </form>
+      <header className="App-header">
+        <button onClick={addDoc} style={{ display: `${isHide ? 'none' : 'block'}` }}>New Game</button>
+        <p>GameID: {gameId}</p>
+        <form id='form' onSubmit={getGameId} style={{ display: `${isHide ? 'none' : 'block'}` }}>
+          <input type='number' placeholder='GameID: ' name='gameidInput' />
+          <input type='text' placeholder='username: ' name='username' />
+          <input type='submit' />
+          <p>Players: </p>
+        </form>
+      </header>
     </div>
+
   );
+  
+  function hide() {
+    setIsHide(true)
+  }
+  function getGameId(e) {
+    e.preventDefault();
+    let username = e.target.children.username.value;
+    let gameId = e.target.children.gameidInput.value;
+    console.log(username)
+    let UID = Math.random().toString(36).substr(2, 9);
+    console.log(UID)
+    sessionStorage.setItem('UID',UID)
+    DB.collection('games').doc(gameId).get().then(e=>{
+      let players= e.data().players;
+      
+      players[UID] = {username}
+      DB.collection('games').doc(gameId).update({players:players})
+      console.log(e.data())
+
+    })
+    hide()
+  }
+  function addDoc() {
+
+    number = Math.floor(Math.random() * 9000) + 1000;
+    number = number.toString()
+    setGameId(number);
+
+    DB.collection('games').doc(number).set({ gameId: number,players:{}})
+      .then((game) => {
+        console.log(game.id)
+        
+      })
+      .catch(e => { console.error(e) });
+
+
+  }
+
+}
+
+function print(){
+  document.getElementById('username')
 }
 
 export default App;
-
-function createUid() {
-  return Math.random().toString(16).slice(2)
-}
-
