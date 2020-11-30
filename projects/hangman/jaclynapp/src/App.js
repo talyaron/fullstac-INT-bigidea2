aa3e8b556abd056e8e49378e04537f496c4bd56d
Jaclyn <jaclynjosephson@gmail.com>
2020-11-30 10:40:26 +0200

    efwf

--- projects/hangman/jaclynapp/src/App.js
+++ projects/hangman/jaclynapp/src/App.js
@@ -2,71 +2,185 @@ import './App.css';
 import { DB } from './Firebase/firebaseConfig';
 import { useState, useEffect } from 'react';
 
-let number;
-
 function App() {
   const [gameId, setGameId] = useState('');
-  const [isHide, setIsHide] = useState(false);
+  const [showCreateGame, setShowCreateGame] = useState(true);
+  const [playersDom, setPlayersDom] = useState([])
+
+
+
+
+
+  function handleNewGame() {
+    let randomId = Math.floor(Math.random() * 10000 + 1);
+    setGameId(randomId);
+    DB.collection('games').add({ gameId: randomId.toString() })
+      .then((game) => { console.log(game.id) })
+      .catch(e => { console.error(e) })
+    setShowCreateGame(false)
+  }
+
+  function joinGame(e) {
+    try {
+      e.preventDefault();
+
+      //get user name and game id from the user
+      let gameId = e.target.children.gameid.value;
+      let username = e.target.children.username.value;
+
+      //create user id, and store it on the sessionStorage
+
+      let userId = getUserUID();
+
+      console.log('before getting the things from db')
+      //get game from the db, and store user in players
+      DB.collection('games')
+        .where('gameId', '==', gameId.toString())
+        .limit(1)
+        .get()
+        .then(gamesDB => {
+          if (gamesDB.size === 0) {
+            alert("A game with this id dosn't exists")
+          } else {
+
+            //set user to game on DB
+
+            gamesDB.forEach(gameDB => {
+              console.log(gameDB.data())
+
+              //extract players
+              let { players } = gameDB.data();
+
+              console.log(players)
+              //if players do not exist in DB
+              if (players === undefined) {
+                players = {};
+
+              }
+
+              //add the user to the players
+              players[userId] = { username };
+
+
+
+              console.log(players);
+
+              listenToPlayers(gameDB.id, setPlayersDom);
+              
+              sessionStorage.setItem('gameUniqueId', gameDB.id)
+
+              //store the players back to DB
+              DB.collection('games').doc(gameDB.id).update({ players })
+                .then(() => { console.log('stored player', userId, 'in game number', gameDB.id) })
+                .catch(e => {
+                  console.error(e)
+                })
+            })
+          }
+
+        })
+        .catch(e => {
+          console.error(e)
+        })
+    } catch (e) {
+      console.error(e)
+    }
+
+  }
+
+
   return (
-
-
-
     <div className="App">
-      <header className="App-header">
-        <button onClick={addDoc} style={{ display: `${isHide ? 'none' : 'block'}` }}>New Game</button>
-        <p>GameID: {gameId}</p>
-        <form id='form' onSubmit={getGameId} style={{ display: `${isHide ? 'none' : 'block'}` }}>
-          <input type='number' placeholder='GameID: ' name='gameidInput' />
-          <input type='text' placeholder='username: ' name='username' />
-          <input type='submit' />
-          <p>Players: </p>
-        </form>
-      </header>
+      <button id="submit" onClick={handleNewGame} className={showCreateGame ? '' : 'hidden'}>
+        Generate Game Id and Submit
+      </button>
+      <p id="gameId>"> Game ID:{gameId} </p>
+      <form onSubmit={joinGame} >
+        <input type='text' name='gameid' placeholder='Enter game number' />
+        <input type='text' name='username' placeholder='Enter your name' />
+        <button type='submit'>Join</button>
+      </form>
+      {playersDom.map((player, index) => {
+        return (<p key={index}>{player.username}</p>)
+      })}
+      <button onClick={() => { createTwoGroups( playersDom) }}>Create groups</button>
     </div>
-
   );
-  
-  function hide() {
-    setIsHide(true)
-  }
-  function getGameId(e) {
-    e.preventDefault();
-    let username = e.target.children.username.value;
-    let gameId = e.target.children.gameidInput.value;
-    console.log(username)
-    let UID = Math.random().toString(36).substr(2, 9);
-    console.log(UID)
-    sessionStorage.setItem('UID',UID)
-    DB.collection('games').doc(gameId).get().then(e=>{
-      let players= e.data().players;
-      
-      players[UID] = {username}
-      DB.collection('games').doc(gameId).update({players:players})
-      console.log(e.data())
-
-    })
-    hide()
-  }
-  function addDoc() {
-
-    number = Math.floor(Math.random() * 9000) + 1000;
-    number = number.toString()
-    setGameId(number);
-
-    DB.collection('games').doc(number).set({ gameId: number,players:{}})
-      .then((game) => {
-        console.log(game.id)
-        
-      })
-      .catch(e => { console.error(e) });
-
-
-  }
-
-}
-
-function print(){
-  document.getElementById('username')
 }
 
 export default App;
+
+// function createUid() {
+//   return Math.random().toString(16).slice(2)
+// }
+
+const uid = function () {
+  return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+
+//returns the uid
+function getUserUID() {
+  //get the uid
+  let userUID = sessionStorage.getItem('userUID');
+  if (userUID === null) {
+    userUID = uid();
+    //create a uid
+    sessionStorage.setItem('userUID', userUID);
+  }
+  return userUID;
+}
+
+
+function listenToPlayers(gameUniqueId, setPlayersDom) {
+  DB.collection('games').doc(gameUniqueId).onSnapshot(gameDB => {
+
+    const players = gameDB.data().players
+
+
+    const playersArr = [];
+    for (let i in players) {
+      playersArr.push({username:players[i].username, userId:i})
+    }
+
+    setPlayersDom(playersArr)
+
+  })
+}
+
+function createTwoGroups(playersDom) {
+
+  let players = [...playersDom], group1 = [], group2 = [], counter = playersDom.length + 2;
+
+  let turn = 0;
+
+  
+
+  while (players.length > 0 && counter>0) {
+
+    let randomPlayer = Math.floor(Math.random() * players.length);
+
+    
+    counter--
+
+    console.log(players.length, counter)
+    if (turn === 0) {
+      group1.push(players[randomPlayer]);
+      players.splice(randomPlayer, 1)
+      turn = 1;
+
+    } else {
+      group2.push(players[randomPlayer]);
+      players.splice(randomPlayer, 1)
+      turn = 0;
+    }
+  }
+  console.log(group1)
+  console.log(group2)
+
+  //store it to DB
+  let gameUniqueId = sessionStorage.getItem('gameUniqueId')
+
+  DB.collection('games').doc(gameUniqueId).update({group1, group2})
+  .then(()=>{console.info('updated groups to DB')})
+  .catch(e=>{console.error(e)})
+}
