cb74c33178c9d88d72171168dfbb1dcdf5f4f862
Abby <abbymiller0123@gmail.com>
2020-12-02 13:50:01 +0200

    a

---
+++ react2/recap2/abbyrecap2/src/Components/FoodCard/FoodCard.js
@@ -0,0 +1,11 @@
+import './FoodCard.css'
+
+function FoodCard(props) {
+    const {food} = props;
+    return(<div className='foodCard'>
+        {food}
+    </div>
+    )
+}
+
+export default FoodCard;
---
+++ react2/recap2/abbyrecap2/src/Main.js
@@ -0,0 +1,45 @@
+import './Main.css';
+import Header from './Components/Header/Header';
+import {useState, useEffect} from 'react'
+import FoodCard from './Components/FoodCard/FoodCard';
+import FirebaseConfig from './Components/FirebaseConfig';
+import {DB} from './Components/FirebaseConfig';
+function Main() {
+  
+  useEffect(()=>{
+    DB.collection('foods').onSnapshot(foodsDB=>{
+      foodsDB.forEach(foodDB=>{
+        const foodsTemp = [];
+        foodsDB.forEach(foodDB =>{
+          foodsTemp.push(foodDB.data().name)
+        })
+        setFoods(foodsTemp)
+      })
+    })
+  }, [])
+  //useEffect: to run something once when you start 
+
+  const [name, setName] = useState('Abby')
+  const [foods, setFoods] = useState(['falafel', 'pizza', 'sushi', 'sabich'])
+
+  function handleInput(e) {
+    //don't need e.preventDefault bc is not onSubmit
+    const value = e.target.value;
+    setName(value);
+  }
+
+  console.log(foods)
+    return (
+        <div className="Main">
+            <Header title='Hello' name={name}/>
+            <input type="text" placeholder="enter name" onKeyUp={handleInput}/>
+          <h2>Foods of Israel</h2>
+          {foods.map((food, index)=>{
+            return <FoodCard key={index} food={food}/>
+          })}
+        </div>
+        //onKeyup is the event listener
+    );
+}
+
+export default Main;
