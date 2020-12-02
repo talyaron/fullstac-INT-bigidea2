26d0366963dd1b6ff8e1c5318bb63c36e1c43b71
Tal <tal.yaron@gmail.com>
2020-12-02 11:54:33 +0200

    firebase

--- react2/recap2/tal/src/App.js
+++ react2/recap2/tal/src/App.js
@@ -1,11 +1,16 @@
 import {useState} from 'react';
 import './App.css';
 import Header from './view/comp/Header/Header';
+import FoodCard from './view/comp/FoodCard/FoodCard';
+
+
+
 
 function App() {
 
 
   const [name, setName] = useState('Jacob');
+  const [foods, setFoods] = useState(['Falalfel', 'Pizza', 'Sushi','Sabich', 'Shaurma']);
 
   function handleInput(e){
     const value = e.target.value;
@@ -17,6 +22,13 @@ function App() {
     <div className="App">
       <Header title='Hello' name={name}/>
       <input type='text' placeholder='Enter a name' onKeyUp={handleInput}/>
+      <h2>Best foods of Israel</h2>
+      {foods.map((food, index)=>{
+         return <FoodCard key={index} food={food} />
+      })
+
+      }
+
     </div>
   );
 }
