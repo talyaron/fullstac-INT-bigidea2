6f52c3fb4477d0ef4318ae57f47c9b1aba43c038
Tal <tal.yaron@gmail.com>
2020-12-02 14:30:08 +0200

    react-router

--- react2/recap2/tal/src/App.js
+++ react2/recap2/tal/src/App.js
@@ -1,36 +1,55 @@
-import {useState} from 'react';
 import './App.css';
-import Header from './view/comp/Header/Header';
-import FoodCard from './view/comp/FoodCard/FoodCard';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link, useParams
+} from "react-router-dom";
 
+import Main from './Main';
+import Suggestions from './view/pages/Suggestions/Suggestions';
 
-
-
-function App() {
-
-
-  const [name, setName] = useState('Jacob');
-  const [foods, setFoods] = useState(['Falalfel', 'Pizza', 'Sushi','Sabich', 'Shaurma']);
-
-  function handleInput(e){
-    const value = e.target.value;
-
-    setName(value);
-  }
-
+export default function App() {
   return (
-    <div className="App">
-      <Header title='Hello' name={name}/>
-      <input type='text' placeholder='Enter a name' onKeyUp={handleInput}/>
-      <h2>Best foods of Israel</h2>
-      {foods.map((food, index)=>{
-         return <FoodCard key={index} food={food} />
-      })
+    <Router>
+      <div>
+        <nav>
 
-      }
+          <div>
+            <Link to="/">Main bla bla</Link>
+          </div>
+          <div>
+            <Link to="/my-thoughts-about-you/">My Thoughts about You</Link>
+          </div>
+          <div>
+            <Link to="/suggestions/">Sugesstions</Link>
+          </div>
 
-    </div>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+
+          <Route path="/my-thoughts-about-you/:id">
+            <About />
+          </Route>
+          <Route path="/suggestions/:id">
+            <Suggestions />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+
+function About() {
+  const { id } = useParams();
+  return <h2>About: {id}</h2>;
+}
+
