c6fb9b10b0fe363e1248fd31add12d3bbca1cd0d
Tal <tal.yaron@gmail.com>
2020-12-13 11:43:00 +0200

    cont

--- react3/tal/src/App.js
+++ react3/tal/src/App.js
@@ -2,18 +2,31 @@ import { useState } from 'react'
 import './App.css';
 
 function App() {
-  const [emojies, setEmojies] = useState([])
+  const [emotions, setemotions] = useState([])
 
 
   function handleAddEmoji(e) {
     e.preventDefault();
-    console.log(e.target.children.emoji.value)
-    setEmojies([...emojies, {
+
+    setemotions([...emotions, {
       text: e.target.children.emoji.value,
       type: 'emoji'
     }])
 
     e.target.children.emoji.value = '';
+  }
+
+  function handleAddSentence(e) {
+
+    e.preventDefault();
+
+    setemotions([...emotions, {
+      text: e.target.children.sentence.value,
+      type: 'sentence'
+    }])
+
+    e.target.children.sentence.value = '';
+
   }
 
 
@@ -22,10 +35,18 @@ function App() {
       <form onSubmit={handleAddEmoji}>
         <input type='text' name='emoji' placeholder='Add emoji' />
       </form>
+      <form onSubmit={handleAddSentence}>
+        <input type='text' name='sentence' placeholder='Add sentence' />
+      </form>
 
-      <h2>Emojies</h2>
-      {emojies.map((emoji, index) => {
+      <h2>emotions</h2>
+      {emotions.map((emoji, index) => {
         return (<img className='emoji' key={index} src={emoji.text} alt='emoji' />)
+      })
+      }
+      <h2>sentences</h2>
+      {emotions.map((emoji, index) => {
+        return (<img className='sentence' key={index} src={emoji.text} alt='emoji' />)
       })
       }
     </div>
