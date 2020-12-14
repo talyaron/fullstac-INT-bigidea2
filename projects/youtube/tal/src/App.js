d06df4b68a22b4a3de0c608a5f0b51367f5f3628
Tal <tal.yaron@gmail.com>
2020-12-14 13:03:38 +0200

    delete

--- projects/youtube/tal/src/App.js
+++ projects/youtube/tal/src/App.js
@@ -1,38 +1,40 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
-import {DB} from './firebaseConfig';
+import { DB } from './firebaseConfig';
 
 function App() {
-//states
+  //states
   const [vidoeUrl, setVideoUrl] = useState('HEQcQm21DlY');
+  const [videos, setVideos] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     const userId = getUserUID();
-   
-    DB.collection('lists').doc(userId).collection('videos').onSnapshot(videosDB=>{
-     
-      let videos = [];
-      videosDB.forEach(videoDB=>{
-        videos.push(videoDB.data())
+
+    DB.collection('lists').doc(userId).collection('videos').onSnapshot(videosDB => {
+
+      let tmpVideos = [];
+      videosDB.forEach(videoDB => {
+        tmpVideos.push(videoDB.data())
       })
-      console.log(videos)
+      console.log()
+      setVideos(tmpVideos)
     })
-  },[])
+  }, [])
 
   function handleUrl(e) {
     e.preventDefault()
-    
+
     let id = e.target.children.url.value;
     let newId = id
-   
+
     if (id.startsWith('https://www.youtube.com/watch?v=')) {
-      newId =  id.slice(32, 43)
-      
+      newId = id.slice(32, 43)
+
     } else if (id.startsWith('youtube.com/watch?v=')) {
-    
-      newId =  id.slice(20, 31)
-     
+
+      newId = id.slice(20, 31)
+
     }
 
     console.log(newId)
@@ -41,12 +43,23 @@ function App() {
     const userId = getUserUID();
     //save to DB
     DB.collection('lists').doc(userId)
-    .collection('videos').doc(newId).set({videoId:newId, date:new Date()})
-    .then(()=>{console.log(`Video was saved for user ${userId}, under Id ${newId}`)})
-    .catch(e=>console.error(e))
+      .collection('videos').doc(newId).set({ videoId: newId, date: new Date() })
+      .then(() => { console.log(`Video was saved for user ${userId}, under Id ${newId}`) })
+      .catch(e => console.error(e))
 
   }
-  
+
+  function deleteVideo(videoId){
+    
+    const userId = getUserUID();
+    DB.collection('lists').doc(userId)
+    .collection('videos')
+    .doc(videoId)
+    .delete()
+    .then(()=>{console.log('delted video', videoId)})
+    .catch(e=>{console.error(e)})
+  }
+
 
   return (
     <div className="App">
@@ -55,7 +68,18 @@ function App() {
 
       </form>
       <header className="App-header">
-        <iframe title='some video' width="560" height="315" src={`https://www.youtube.com/embed/${vidoeUrl}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        {
+          videos.map((video, index) => {
+            console.log(video)
+            return (
+              <div>
+                <iframe key={index} title='some video' width="560" height="315" src={`https://www.youtube.com/embed/${video.videoId}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <button onClick={()=>{deleteVideo(video.videoId)}}>Delete</button>
+              </div>
+              )
+          })
+
+        }
       </header>
     </div>
   );
@@ -69,12 +93,12 @@ const uid = function () {
 
 //returns the uid
 function getUserUID() {
-    //get the uid
-    let userUID = sessionStorage.getItem('userUID');
-    if (userUID === null) {
-        userUID = uid();
-        //create a uid
-        sessionStorage.setItem('userUID', userUID);
-    }
-    return userUID;
+  //get the uid
+  let userUID = sessionStorage.getItem('userUID');
+  if (userUID === null) {
+    userUID = uid();
+    //create a uid
+    sessionStorage.setItem('userUID', userUID);
+  }
+  return userUID;
 }
