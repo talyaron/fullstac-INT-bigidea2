a05c41b51db0ec54460c1792a6c9f2eb749140b5
Ak <banjothere@gmail.com>
2020-12-16 01:33:07 -0800

    hjk
    aryeh

---
+++ react3/aryeh11111/src/Main.js
@@ -0,0 +1,106 @@
+import './Main.css';
+import { useState, useEffect } from 'react';
+import { useParams } from "react-router-dom";
+
+import { DB } from './firebaseConfig';
+
+function Main() {
+
+    let { userId } = useParams();
+    console.log(userId)
+
+    //states
+    const [vidoeUrl, setVideoUrl] = useState('HEQcQm21DlY');
+    const [videos, setVideos] = useState([])
+
+    useEffect(() => {
+        const userId = getUserUID();
+
+        DB.collection('lists').doc(userId).collection('videos').onSnapshot(videosDB => {
+
+            let tmpVideos = [];
+            videosDB.forEach(videoDB => {
+                tmpVideos.push(videoDB.data())
+            })
+            console.log()
+            setVideos(tmpVideos)
+        })
+    }, [])
+
+    function handleUrl(e) {
+        e.preventDefault()
+
+        let id = e.target.children.url.value;
+        let newId = id
+
+        if (id.startsWith('https://www.youtube.com/watch?v=')) {
+            newId = id.slice(32, 43)
+
+        } else if (id.startsWith('youtube.com/watch?v=')) {
+
+            newId = id.slice(20, 31)
+
+        }
+
+        console.log(newId)
+        setVideoUrl(newId)
+
+        const userId = getUserUID();
+        //save to DB
+        DB.collection('lists').doc(userId)
+            .collection('videos').doc(newId).set({ videoId: newId, date: new Date() })
+            .then(() => { console.log(`Video was saved for user ${userId}, under Id ${newId}`) })
+            .catch(e => console.error(e))
+
+    }
+
+    function deleteVideo(videoId) {
+
+        const userId = getUserUID();
+        DB.collection('lists').doc(userId)
+            .collection('videos')
+            .doc(videoId)
+            .delete()
+            .then(() => { console.log('delted video', videoId) })
+            .catch(e => { console.error(e) })
+    }
+
+
+    return (
+        <div className="App">
+            <form onSubmit={handleUrl} className='inputTop'>
+                <input type='text' name='url' />
+
+            </form>
+            <header className="App-header">
+                {
+                    videos.map((video, index) => {
+                        console.log(video)
+                        return (
+                            <div>
+                                <iframe key={index} title='some video' width="560" height="315" src={`https://www.youtube.com/embed/${video.videoId}`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                <button onClick={() => { deleteVideo(video.videoId) }}>Delete</button>
+                            </div>
+                        )
+                    })
+                }
+            </header>
+        </div>
+    );
+}
+
+const uid = function () {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+//returns the uid
+function getUserUID() {
+    //get the uid
+    let userUID = sessionStorage.getItem('userUID');
+    if (userUID === null) {
+        userUID = uid();
+        //create a uid
+        sessionStorage.setItem('userUID', userUID);
+    }
+    return userUID;
+}
+export default Main;
