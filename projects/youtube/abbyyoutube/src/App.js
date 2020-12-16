9a8cb4578001b69f4ede75e76e1bd677919bbada
Abby <abbymiller0123@gmail.com>
2020-12-16 11:41:59 +0200

    abby

--- projects/youtube/abbyyoutube/src/App.js
+++ projects/youtube/abbyyoutube/src/App.js
@@ -1,108 +1,33 @@
 import './App.css';
-import { DB } from './Firebase';
-import { useState, useEffect } from 'react';
+//import { DB } from './Firebase';
+//import { useState, useEffect } from 'react';
+import Code from './Code'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
-
-  const [links, setLinks] = useState([])
-  const [linksFirebase, setLinksFirebase] = useState([])
-  const [userId, setUserId] = useState('')
-  const [linkId, setLinkId] = useState('')
-  
-  useEffect(() => {
-    const userId = getUserUID();
-
-    DB.collection('youtube').doc(userId).collection('videos').onSnapshot(videosDB => {
-      let videoArray = [];
-      videosDB.forEach(videoDB => {
-        videoArray.push(videoDB.data())
-      })
-      setLinksFirebase(videoArray)
-      console.log("videoArray" + videoArray)
-    })
-  }, [])
-
-  function handleSubmitId(e) {
-    
-    e.preventDefault()
-    console.log(e.target.children.input.value)
-    if (e.target.children.input.value.startsWith('https://www.youtube.com/watch?v=', 0)) {
-      handleLinkSeg(e.target.children.input.value.slice(32))
-    } else if (e.target.children.input.value.startsWith('youtube.com/watch?v=', 0)) {
-      handleLinkSeg(e.target.children.input.value.slice(20))
-    }
-    else {
-      handleLinkSeg(e.target.children.input.value)
-    }
-  }
-
-  function handleLinkSeg(submittedValue) {
-    let link = submittedValue
-    setLinkId(submittedValue)
-    console.log(link);
-    //DB.collection('youtube').add({ linkSegment: link })
-    //document.getElementById('iframeId').src = "https://www.youtube.com/embed/" + link
-    let fullLink = "https://www.youtube.com/embed/" + link
-    setLinks([...links, fullLink])
-    console.log(links)
-    DB.collection('youtube').doc(userId)
-    .collection('videos').doc(link).set({ videoId: link, date: new Date() })
-    .then(() => { console.log(`Video saved; user: ${userId}; Id: ${link}`) })
-    .catch(e => console.error(e))
-  }
-  function handleDelete(Id) {
-    const userId = getUserUID();
-    DB.collection('youtube').doc(userId).collection(Id).doc()
-    .delete()
-    .then(()=>{console.log('delted video', linkId)})
-    .catch(e=>{console.error(e)})
-  }
-  /*function handleDelete(e, Id) {
-    e.preventDefault()
-    console.log(e.target.name)
-    let index = e.target.name
-    document.getElementById(index).remove()
-  }*/
-  const uid = function () {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
-  }
-  function getUserUID() {
-    //get the uid
-    let userUID = sessionStorage.getItem('userUID');
-    if (userUID === null) {
-        userUID = uid();
-        //create a uid
-        sessionStorage.setItem('userUID', userUID);
-    }
-    return userUID;
-}
-useEffect(() => {
-  const userIdVal = getUserUID();
-setUserId(userIdVal)
-}, [])
 
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmitId}>
-        <input id='input' type='text' name='input' placeholder='enter youtube video id'></input>
-      </form>
-      <div id="app-header">
-        {
-          links.map((link, index) => {
-            return (<div id={index} key={index}>
-              <p>userId: {userId}</p>
-              <iframe id="iframeId" width="560" height="315" src={link}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
-    gyroscope; picture-in-picture" allowFullScreen></iframe>
-              <div><input type='submit' value='delete' className='deleteButton' name={index} onClick={()=>{handleDelete({linkId})}}/></div>
-            </div>)                 
-          })
-        }
-
+       <Router>
+      <div>
+        <Switch>
+          <Route path="/:userId">
+            <Code />
+          </Route>
+          <Route path="/">
+            <Code />
+          </Route>
+        </Switch>
       </div>
+    </Router>
     </div >
   );
 }
 
 export default App;
+
---
+++ projects/youtube/abbyyoutube/src/Code.js
@@ -0,0 +1,105 @@
+import './Code.css';
+import { DB } from './Firebase';
+import { useState, useEffect } from 'react';
+
+
+function Code() {
+
+    const [linkId, setLinkId] = useState('')
+    const [links, setLinks] = useState([])
+    const [linksFirebase, setLinksFirebase] = useState([])
+    const [userId, setUserId] = useState('')
+
+    useEffect(() => {
+        const userId = getUserUID();
+        setUserId(userId)
+
+        DB.collection('youtube').doc(userId).collection('videos').onSnapshot(videosDB => {
+            let videoArray = [];
+            videosDB.forEach(videoDB => {
+                let vidId = videoDB.data().videoId
+                //console.log("https://www.youtube.com/watch?v="+vidId)
+                videoArray.push("https://www.youtube.com/watch?v=" + vidId)
+                console.log("videoArray" + videoArray)
+            })
+            setLinksFirebase(videoArray)
+            //console.log("videoArray" + videoArray)
+        })
+
+    }, [])
+
+
+    function handleSubmitId(e) {
+        e.preventDefault()
+        console.log(e.target.children.input.value)
+        if (e.target.children.input.value.startsWith('https://www.youtube.com/watch?v=', 0)) {
+            handleLinkSeg(e.target.children.input.value.slice(32))
+        } else if (e.target.children.input.value.startsWith('youtube.com/watch?v=', 0)) {
+            handleLinkSeg(e.target.children.input.value.slice(20))
+        }
+        else {
+            handleLinkSeg(e.target.children.input.value)
+        }
+    }
+
+    function handleLinkSeg(submittedValue) {
+        let link = submittedValue
+        setLinkId(link)
+        console.log(link);
+        //DB.collection('youtube').add({ linkSegment: link })
+        //document.getElementById('iframeId').src = "https://www.youtube.com/embed/" + link
+        let fullLink = "https://www.youtube.com/embed/" + link
+        setLinks([...links, fullLink])
+        console.log(links)
+        DB.collection('youtube').doc(userId)
+            .collection('videos').doc(link).set({ videoId: link, date: new Date() })
+            .then(() => { console.log(`Video saved; user: ${userId}; Id: ${link}`) })
+            .catch(e => console.error(e))
+    }
+    function handleDelete(Id) {
+        const userId = getUserUID();
+        DB.collection('youtube').doc(userId).collection(Id).doc()
+            .delete()
+            .then(() => { console.log('delted video', linkId) })
+            .catch(e => { console.error(e) })
+    }
+
+    const uid = function () {
+        return Date.now().toString(36) + Math.random().toString(36).substr(2);
+    }
+    function getUserUID() {
+        //get the uid
+        let userUID = sessionStorage.getItem('userUID');
+        if (userUID === null) {
+            userUID = uid();
+            //create a uid
+            sessionStorage.setItem('userUID', userUID);
+        }
+        return userUID;
+    }
+
+    return (
+        <div className="Code">
+            <form onSubmit={handleSubmitId}>
+                <input id='input' type='text' name='input' placeholder='enter youtube link or video id'></input>
+            </form>
+            <div id="app-header">
+                {
+                    linksFirebase.map((link, index) => {
+                        return (<div id={index} key={index}>
+                            <p>userId: {userId}</p>
+                            <div>
+                                <iframe id="iframeId" width="560" height="315" src={link}
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+gyroscope; picture-in-picture" allowFullScreen></iframe>
+                            </div>
+                            <div><input type='submit' value='delete' className='deleteButton' name={index} onClick={() => { handleDelete({ linkId }) }} /></div>
+                        </div>)
+                    })
+                }
+
+            </div>
+        </div >
+    );
+}
+export default Code;
